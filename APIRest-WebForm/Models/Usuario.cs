434d54f78ea5aef60bc00ef96c7d382d9fb9c6e8
DaniBonica001 <danielabc_0106@hotmail.com>
2023-12-13 23:56:04 -0500

    Added: Context and Model

---
+++ APIRest-WebForm/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIRest_WebForm.Models
+{
+    public partial class Usuario
+    {
+        public decimal Identificador { get; set; }
+        public string? Usuario1 { get; set; }
+        public string? Pass { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
